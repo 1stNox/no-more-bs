5c6c229ce0f23633643ee0b57e5ebda8b5b9852d
Finn-Lasse Reichling <finn.lasse.reichling@gmail.com>
2026-05-09 17:37:50 +0200

    feat: add file and dir sha256 hashing

---
+++ src/lib/hash.ts
@@ -0,0 +1,32 @@
+import crypto from "crypto";
+import fs from "fs";
+import path from "path";
+
+export function hashFile(filePath: string): string {
+  return crypto.createHash("sha256").update(fs.readFileSync(filePath)).digest("hex");
+}
+
+export function hashDir(dirPath: string): string {
+  const files: string[] = [];
+
+  const walk = (cur: string) => {
+    for (const ent of fs.readdirSync(cur, { withFileTypes: true })) {
+      const abs = path.join(cur, ent.name);
+      if (ent.isDirectory()) walk(abs);
+      else if (ent.isFile()) files.push(abs);
+    }
+  };
+
+  walk(dirPath);
+  files.sort();
+
+  const h = crypto.createHash("sha256");
+  for (const abs of files) {
+    const rel = path.relative(dirPath, abs);
+    h.update(rel);
+    h.update("\0");
+    h.update(hashFile(abs));
+    h.update("\0");
+  }
+  return h.digest("hex");
+}
